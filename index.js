0f858a071189fe887d81adfa32d99a9bf6f0eac1
Sagnik Ghosh <sagnikghosh@Sagniks-MacBook-Air.local>
2024-06-14 18:29:13 +0530

    pushed basic server code

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('Twitter')
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1>please login</h1>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
